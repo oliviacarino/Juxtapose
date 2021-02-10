6315d7e497b9407a27296ea52b936c66737f71a9
Olivia <51516158+oliviacarino@users.noreply.github.com>
2021-02-09 21:18:31 -0500

    Tried to add images but removed them from cat pages

---
+++ front-page.php
@@ -0,0 +1,390 @@
+<?php
+get_header();
+?>
+<article class="content mt-0 px-3 mx-1 p-md-4">
+
+    <!-- Container -->
+    <div class="container">
+        <!-- Row 1 -->
+        <div class="row">
+            <div class="col-3">
+                <div class="shadow-sm card frontpage-div-header">
+                    <h4 class="card-header text-center" style="color: black;">Top Stories</h4>
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'top-posts',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 3
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body">
+                        <div class="latest_news_cont">
+                            <a href="<?php the_permalink() ?>">
+                                <?php //the_post_thumbnail(); ?>
+                            </a>
+
+                            <a href="<?php the_permalink() ?>">
+                                <h5 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h5>
+                            </a>
+                            <?php //the_excerpt(); ?>
+                            <!-- <p><a href="<?php the_permalink() ?>" class="readmore_news">Read more ></a></p> -->
+                            <?php echo get_the_date( 'F j, Y' ); ?>
+                        </div>
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card text-left shadow-sm">
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'promo-stories',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 1
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body">
+                        <div class="latest_news_cont">
+                            <!-- <a href="<?php the_permalink() ?>"> -->
+                            <div class="mb-2">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <!-- </a> -->
+
+                            <a href="<?php the_permalink() ?>">
+                                <h4 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h4>
+                            </a>
+                            <?php //the_excerpt(); ?>
+                            <div class="pt-5">
+                                <?php echo get_the_date( 'F j, Y' ); ?>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+
+            <div class="col-3">
+                <?php get_sidebar( 'one' ); ?>
+            </div>
+
+        </div>
+
+        <!-- Row 2 -->
+        <div class="row">
+            <div class="col-3">
+                <div class="card text-left shadow-sm mt-4">
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'promo-column',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 1
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body">
+                        <div class="latest_news_cont">
+                            <!-- <a href="<?php the_permalink() ?>"> -->
+                            <div class="mb-2">
+                                <div class="thumbnail-size mb-4">
+                                    <?php the_post_thumbnail('medium'); ?>
+                                </div>
+                            </div>
+                            <!-- </a> -->
+
+                            <a href="<?php the_permalink() ?>">
+                                <h4 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h4>
+                            </a>
+                            <?php //the_excerpt(); ?>
+                            <div class="pt-5">
+                                <?php echo get_the_date( 'F j, Y' ); ?>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+
+            <div class="col-6">
+                <div class="card text-left shadow-sm mt-4 mb-4">
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'promo-stories2',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 1
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body">
+                        <div class="latest_news_cont">
+                            <!-- <a href="<?php the_permalink() ?>"> -->
+                            <div class="mb-2">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <!-- </a> -->
+
+                            <a href="<?php the_permalink() ?>">
+                                <h4 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h4>
+                            </a>
+                            <?php the_excerpt(); ?>
+                            <div class="pt-5">
+                                <?php echo get_the_date( 'F j, Y' ); ?>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+            <div class="col-3" id="sidebar-two">
+                <?php get_sidebar( 'two' ); ?>
+            </div>
+
+
+
+
+        </div>
+    </div>
+
+    <!-- Container 2 - Ad Space -->
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <div class="card text-center shadow-sm py-3">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color: black;">Ad Space</h5>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+    <!-- Container 3 -->
+    <div class="container">
+        <div class="row mt-4">
+
+            <!-- Local News Div -->
+            <div class="col-3">
+                <div class="card text-left shadow-sm pb-4">
+                    <div class="card-header text-center sabon-font frontpage-div-header">
+                        <strong>Local News</strong>
+                    </div>
+
+                    <img class="card-img-top" src="wp-content/themes/juxtaposeAlpha/assets/images/peopleWalking.jpg" alt="Card image cap">
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'local-news',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 3,
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body pb-0">
+                        <div class="latest_news_cont">
+                            <a href="<?php the_permalink() ?>">
+                                <?php the_post_thumbnail(); ?>
+                            </a>
+
+                            <a href="<?php the_permalink() ?>">
+                                <h5 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h5>
+                            </a>
+                            <?php //the_excerpt(); ?>
+                            <!-- <p><a href="<?php the_permalink() ?>" class="readmore_news">Read more ></a></p> -->
+                            <div class="mb-1 pb-2">
+                                <?php echo get_the_date( 'F j, Y' ); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+
+            <!-- Sports Div -->
+            <div class="col-3">
+                <div class="card text-left shadow-sm">
+                    <div class="card-header text-center sabon-font frontpage-div-header">
+                        <strong>Sports</strong>
+                    </div>
+
+                    <img class="card-img-top" src="wp-content/themes/juxtaposeAlpha/assets/images/sports.jpg" alt="Card image cap">
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'sports',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 3,
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body pb-0">
+                        <div class="latest_news_cont">
+                            <a href="<?php the_permalink() ?>">
+                                <?php the_post_thumbnail(); ?>
+                            </a>
+
+                            <a href="<?php the_permalink() ?>">
+                                <h5 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h5>
+                            </a>
+                            <?//php the_excerpt(); ?>
+                            <!-- <p><a href="<?php the_permalink() ?>" class="readmore_news">Read more ></a></p> -->
+                            <div class="mb-1 pb-2">
+                                <?php echo get_the_date( 'F j, Y' ); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+
+            <!-- Education Div -->
+            <div class="col-3">
+                <div class="card text-left shadow-sm pb-4">
+                    <div class="card-header text-center sabon-font pt-3 frontpage-div-header">
+                        <strong>Education</strong>
+                    </div>
+
+                    <img class="card-img-top" src="wp-content/themes/juxtaposeAlpha/assets/images/education.jpg" alt="Card image cap">
+
+                    <?php
+                            // the query
+                            $the_query = new WP_Query(array(
+                                'category_name' => 'education',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 3,
+                            ));
+                            ?>
+
+                    <?php if ($the_query->have_posts()) : ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+                    <div class="card-body pb-0">
+                        <div class="latest_news_cont">
+                            <a href="<?php the_permalink() ?>">
+                                <?php the_post_thumbnail(); ?>
+                            </a>
+
+                            <a href="<?php the_permalink() ?>">
+                                <h5 style="color: black;">
+                                    <?php the_title(); ?>
+                                </h5>
+                            </a>
+                            <?//php the_excerpt(); ?>
+                            <!-- <p><a href="<?php the_permalink() ?>" class="readmore_news">Read more ></a></p> -->
+                            <div class="mb-1 pb-2">
+                                <?php echo get_the_date( 'F j, Y' ); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+            <div class="col-3" id="sidebar-three">
+                <?php get_sidebar( 'three' ); ?>
+            </div>
+
+        </div>
+    </div>
+    </div>
+</article>
+
+
+<?php
+get_footer();
+?>
---
+++ single.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ */
+
+get_header();
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-9 mx-auto"> 
+          
+          <section id="primary" class="site-content">
+            <div id="content" role="main">
+              <?php 
+              // Check if there are any posts to display
+              if ( have_posts() ) : ?>
+            <!-- <img class="mx-auto d-block" src="<?php //the_post_thumbnail_url('full'); ?>"> -->
+              <header class="archive-header">
+                <h1 style="font-size: 30px;" class="text-center mb-4"><?php single_cat_title( '', true ); ?></h1>
+                  <?php
+                  // Display optional category description
+                  if ( category_description() ) : ?>
+                  <div class="archive-meta"><?php echo category_description(); ?></div>
+                  <?php endif; ?>
+              </header>
+
+              <?php
+              // The Loop
+              while ( have_posts() ) : the_post(); ?>
+                <h2 class="mt-2">
+                  <a class="category-post-title" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                </h2>
+                <small style="font-size: 15px;"><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+                
+                <div class="entry" style="font-size: 17px;">
+                    <br>
+                <?php the_content(); ?> 
+                  <!-- <p class="postmetadata"> -->
+                  <?php
+                    //comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed');
+                  ?>
+                  <!-- </p> -->
+                </div>
+                <br>
+              <?php endwhile; 
+              else: ?>
+                <p>Sorry, no posts matched your criteria.</p> 
+              <?php endif; ?>
+
+            </div>
+          </section>
+        </div>
+    </div>  
+</div> 
+
+
+
+<?php 
+get_footer();
+?>
